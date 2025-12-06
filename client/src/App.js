0390d76e47a58149a67c17971581ac5682f02145
DrStang <48525932+DrStang@users.noreply.github.com>
2025-12-06 13:43:17 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -114,6 +114,7 @@ function App() {
             element={
               <PrivateRoute isAuthenticated={isAuthenticated}>
                 <NYTPanel />
+              </PrivateRoute>
             }
           />    
           <Route
