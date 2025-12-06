6e1eee7058d01f74abfb6ffb98c0024621349232
DrStang <48525932+DrStang@users.noreply.github.com>
2025-12-06 10:05:36 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AdminDashboard } from './components/Admin';
+import { AdminDashboard } from './components/Admin/AdminDashboard';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
