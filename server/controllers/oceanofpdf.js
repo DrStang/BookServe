f320df4467a885a8f0da9260da6869d311c7da58
DrStang <48525932+DrStang@users.noreply.github.com>
2025-12-16 19:36:20 -0500

    Update oceanofpdf.js

--- server/controllers/oceanofpdf.js
+++ server/controllers/oceanofpdf.js
@@ -7,7 +7,11 @@ const instance = axios.create({
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Language': 'en-US,en;q=0.9'
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://oceanofpdf.com/',
+    'Origin': 'https://oceanofpdf.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive'
   },
   withCredentials: true
 });    
