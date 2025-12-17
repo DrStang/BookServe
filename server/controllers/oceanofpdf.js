c4d11fe7ba296af7612d2c9dde5b6ec407aa045b
DrStang <48525932+DrStang@users.noreply.github.com>
2025-12-16 19:21:44 -0500

    Update oceanofpdf.js

--- server/controllers/oceanofpdf.js
+++ server/controllers/oceanofpdf.js
@@ -2,15 +2,23 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+const instance = axios.create({
+  baseURL:'https://oceanofpdf.com',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'en-US,en;q=0.9'
+  },
+  withCredentials: true
+});    
 async function searchOceanOfPDF(title, author) {
   try {
     // Construct search query URL
     const query = `${title} ${author}`.replace(/\s+/g, '+');
-    const url = `https://oceanofpdf.com/?s=${query}`;
-    console.log(url);
+    const url = `/?s=${query}`;
 
+    const { data, headers } await instance.get(url);
     // Fetch the search results page
-    const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
     // Parse and extract relevant links
@@ -29,15 +37,19 @@ async function searchOceanOfPDF(title, author) {
 }
 async function getBookDetails(bookUrl) {
   try {
-    const { data } = await axios.get(bookUrl);
+    const { data, headers } = await instance.get(bookUrl);
+
     const $ = cheerio.load(data);
 
-    let downloadUrl = null;
-      $('form[action="https://oceanofpdf.com/Fetching_Resource.php"]').each((index, element) => {
+    let pdfDownloadUrl = null;
+    let epubDownloadUrl = null;
+
+    // Extract the form action URL and hidden inputs for both PDF and EPUB
+    $('form[action="https://oceanofpdf.com/Fetching_Resource.php"]').each((index, element) => {
       const id = $(element).find('input[name="id"]').attr('value');
       const filename = $(element).find('input[name="filename"]').attr('value');
 
-       if (id && filename) {
+      if (id && filename) {
         const downloadUrl = `https://oceanofpdf.com/Fetching_Resource.php?id=${id}&filename=${encodeURIComponent(filename)}`;
 
         if (filename.endsWith('.epub')) {
@@ -55,7 +67,6 @@ async function getBookDetails(bookUrl) {
     return null;
   }
 }
-
 async function download(url, filePath) {
   try {
     const { data } = await axios({
