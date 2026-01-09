b5efbb57fad138ac488e1df61d24fd262d3937e7
DrStang <48525932+DrStang@users.noreply.github.com>
2026-01-08 19:21:38 -0500

    Update goodreadsRatings.js

--- server/services/goodreadsRatings.js
+++ server/services/goodreadsRatings.js
@@ -43,7 +43,7 @@ class GoodreadsRatings {
   async searchTitle(title, author) {
     let conn; 
     try {
-      conn = await pool.getConnection();
+      conn = await this.pool.getConnection();
       const query = 'SELECT star_rating, num_ratings FROM Scrape WHERE name = ? AND author = ?';
 
       const rows = await conn.query(query, [title, author]);
