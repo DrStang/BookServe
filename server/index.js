fd9932f51d349d63a9321dee8773d92d51d121e6
DrStang <48525932+DrStang@users.noreply.github.com>
2026-02-12 06:11:39 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -58,8 +58,8 @@ app.use(cors({
   exposedHeaders: ['Content-Type', 'Content-Length'],
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json( { limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 app.use(morgan('dev'));
 
 // API Routes
