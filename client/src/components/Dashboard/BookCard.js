48f38e2efb5baf9468e4cf5f8148d8c69b742747
DrStang <48525932+DrStang@users.noreply.github.com>
2026-04-15 15:34:49 -0400

    Update BookCard.js

--- client/src/components/Dashboard/BookCard.js
+++ client/src/components/Dashboard/BookCard.js
@@ -135,7 +135,7 @@ const BookCard = ({ book, onUpdate, readingProgress, onClick }) => {
 
     setSending(true);
     try {
-      const bookRef = currentBook || book; // currentBook for modal, book for BookCard/BookPage
+      const bookRef = book; // currentBook for modal, book for BookCard/BookPage
       const format = needsEpubConversion(bookRef.format) ? 'epub' : null;
       const response = await emailAPI.sendBook(bookRef.id, email, format, saveEmail);
       
